846bd35a633aa869e80a61f2f10456113821e2e6
Charlie <chaoticcow@gmail.com>
2024-02-04 21:40:37 -0500

    Same as last commit but including changes.

---
+++ Assets/Scripts/Orb.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CharlieMadeAThing.ProjectHex
+{
+    public class Orb : MonoBehaviour {
+        public OrbData OrbData;
+    }
+}
